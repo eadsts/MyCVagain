3decc49de1987081a55711780b306208471c0e7d
Tracy Eads <tracyeads1@yahoo.com>
2020-11-09 16:31:01 -0500

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyCV';
+  name: string = 'Tracy Eads';
+  address: string = '2214 Ross Estates Drive';
+  phone: string = '513-555-5555';
+  email: string = 'tracy@yahoo.com';
+  workexperience: string = 'Miami University, Oxford, Ohio 2016-2020';
+  workexperience2: string = 'Ross High School, Hamilton, Ohio 2015-2016';
+  education: string = 'MAX Technical Training, Mason, Ohio 2020';
+  education2: string = 'University of Cincinnati, Cincinnati, Ohio 1993-1995';
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+  
+
+
+// }
+
+//export class 
